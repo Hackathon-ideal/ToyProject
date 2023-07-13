de52a17445a87106f2571fea5cd12baa8cbeca9f
heeseon1 <heeseon7789@gmail.com>
2023-07-14 01:51:03 +0900

    화면 추가

--- Menu/photo/views.py
+++ Menu/photo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Photo
 
 # Create your views here.
+
+def main(request):
+    return render(request, 'photo/home.html')
+
+def list(request):
+    return render(request, 'photo/list.html')
